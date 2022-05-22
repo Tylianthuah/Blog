7d942379ce53d19b61f7b1bb86c11a217672fc69
Tylianthuah <tylian15@gmail.com>
2022-05-23 00:32:17 +0530

    Quick Read completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,23 @@ const toggleTheme = () => {
 
 themeToggleBtn.addEventListener('click', toggleTheme);
 // Swiper
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  breakpoints: {
+    700: {
+      slidesPerView: 2,
+    },
+
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
